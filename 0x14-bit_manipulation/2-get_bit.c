3f5ffb0e6efe821a54c4cd9cb0c59221d6af01e2
Sbucy <sbucysiwe@gmail.com>
2023-07-06 16:59:03 -0700

    lists

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
+ *converts a binary number to unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
