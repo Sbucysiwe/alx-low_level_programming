1cc4c4cc9b56fcc58cfa4505893f597651d06d46
Sbucy <sbucysiwe@gmail.com>
2023-07-10 16:22:53 -0700

    lists

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /*
- * Read the text file and print 
+* Read the text file and print 
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -9,14 +9,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
+	return (0);
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
-
 	free(buf);
 	close(fd);
 	return (w);
