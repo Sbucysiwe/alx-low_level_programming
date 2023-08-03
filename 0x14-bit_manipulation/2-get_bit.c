7a682b16fb9919f90d6008e7ff85fafb478bdd33
Sbucysiwe <sbucysiwe@gmail.com>
2023-08-03 16:46:43 -0700

    lists

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- *converts a binary number to unsigned int
- */
+*get_bit -Get the bit value at a given index in a decimal number.
+*@n: The decimal number to search the bit in.
+*@index: The index of the bit to get.
+*Return: The value of the bit at the specified index (0 or 1).
+*/
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
