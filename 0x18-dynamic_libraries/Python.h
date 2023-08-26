6279998ea0e638af0eaebd060e2e519a6c0f1483
Sbucysiwe <sbucysiwe@gmail.com>
2023-08-25 19:13:04 -0700

    lists

---
+++ 0x18-dynamic_libraries/Python.h
@@ -0,0 +1,12 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <Python.h>
+
+int add(int a, int b);
+int sub(int a, int b);  
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
